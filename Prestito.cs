37a76184505954c1e8e948714b8ff03eb093bd6a
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-01 12:56:35 +0200

    Costruttore prestito

--- Prestito.cs
+++ Prestito.cs
@@ -8,11 +8,20 @@ namespace csharp_banca_oop
 {
     internal class Prestito
     {
-        Cliente intestatario;
-        int Ammontare { get; set; }
-        int Rata { get; set; }
-        DateTime Inizio { get; set; }
-        DateTime Fine { get; set; }
+        public Prestito(Cliente intestatario, int ammontare, int rata, DateTime inizio, DateTime fine)
+        {
+            Intestatario = intestatario;
+            Ammontare = ammontare;
+            Rata = rata;
+            Inizio = inizio;
+            Fine = fine;
+        }
+
+        public Cliente Intestatario { get; set; }
+        public int Ammontare { get; set; }
+        public int Rata { get; set; }
+        public DateTime Inizio { get; set; }
+        public DateTime Fine { get; set; }
 
         public string Riepilogo ()
         {
