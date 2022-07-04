0a2c3425720fdfb09f3b7fa250a180da519bcc14
giuseppemirizzi <giuseppe.m1995@gmail.com>
2022-07-04 16:16:43 +0200

    creata la classe conto

---
+++ Conto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_banca_oop
+{
+    internal class Conto
+    {
+        public Conto(Cliente intestatario)
+        {
+            Intestatario = intestatario;
+        }
+        public Cliente Intestatario { get; }
+        public decimal Saldo { get; set; }
+    }
+}
